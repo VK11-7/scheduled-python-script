d716dd1e03b8638ae70aef0ce23dcd980af7c903
VK <67276637+VK11-7@users.noreply.github.com>
2025-01-11 19:58:49 +0530

    Update script.py

--- script.py
+++ script.py
@@ -1,4 +1,14 @@
-from datetime import datetime
+import sys
+import time
+from bs4 import BeautifulSoup
+import requests
+import re
+import pandas as pd
+from io import BytesIO
+import urllib
+from datetime import datetime, timedelta
+import streamlit as st
+import docx
 
 def main():
     now = datetime.now()
